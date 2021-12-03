b14a8bf46d868a2591a96844bc7fd9c2579647b3
Alex Cress <alex_cress@hotmail.com>
2021-12-03 11:49:55 -0700

    Initial upload
    -Basic ejs functionality
    -Database calls to retrieve Packages table, "proof of concept" code to show images, but they are not styled

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const mysql = require("mysql");
+const FileReader = require('filereader');
+let fileReader = new FileReader();
+var FileAPI = require('file-api')
+  , File = FileAPI.File
+  , FileList = FileAPI.FileList
+  ;
+
+let table = {};
+app.use(express.static("public", { "extensions": ["css", "js"] }));
+app.use(express.static("views", { "extensions": ["html", "htm", "ejs"] }));
+app.set("view engine", "ejs");
+
+app.listen(8000, ()=>{ console.log("server started on port 8000"); });
+
+var getConnection = ()=>{
+	return mysql.createConnection({
+		host: "localhost",
+		user: "Alex",
+		password: "password",
+		database: "travelexperts"
+	})
+};
+
+app.get("/", (req, res)=>{
+		var connection = getConnection();
+
+		connection.connect( (err)=>{
+
+			if(err){
+				throw err;
+			}
+			var sql = "SELECT * FROM packages";
+			connection.query(sql, (err, result, fields)=>{
+				if(err){
+					throw err;
+				}
+				table = result;	
+			});
+		});
+		
+		res.render("../index", {table : table});
+	});
+
+app.use((req, res, next)=>{
+	res.status(404).render("404");
+});
+
